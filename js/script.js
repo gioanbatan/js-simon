9f8f63fce95df85a527cc520f29ffe43954439e4
gioanbatan <giovannibassani@hotmail.com>
2022-10-19 16:07:49 +0200

    Script: pseudocode created

--- js/script.js
+++ js/script.js
@@ -9,4 +9,31 @@ Consigli del giorno:
 :avviso:  Ricordatevi che oggi pomeriggio ci sarà il recap sulle funzioni :paperella: :party_blob:
 Buon lavoro! :muscolo: */
 
-alert();
+
+// Creare un array per i 5 numeri "numeri casuali"
+// PER ogni indice dell'array generare un numero casuale da 1 a 100
+//      SE un numero generato casualmente è già presente nell'array generarne un altro
+//      Aggiungere il numero all'array
+//      Quando l'array è pieno proseguire
+
+// Mostrare i numeri nella pagina
+// Creare un timer di 30 secondi e avviarlo
+// Al termine del timer nascondere i numeri
+
+// Creare un array per contenere i numeri dati dal giocatore "numeri giocatore" 
+// Chiedere al giocatore di inserire i numeri uno alla volta
+//      SE un numero è gia stato inserito nell'array, chiedere al giocatore di inserire un altro numero
+//      Inserire il singolo numero nell'array
+//      Quando l'array è pieno proseguire
+
+// PER ogni ogni numero dell'array numeri giocatore confrontare ogni numero dell array numeri casuali
+//      SE il singolo numero è presente inserie il numero nell'array numeri indovinati
+//      Terminato il controllo degli array proseguire
+
+// SE l'array numeri indovinati è lungo quanto l'array numeri casuali mostrare il messaggio "HAI VINTO" e tutti i numeri indovinati 
+//      ALTRIMENTI SE l'array numeri indovinati contiene 0 valori mostra il messagio "hai perso, non hai indovinato nessun numero"
+//      ALTRIMENTI SE l'array numeri indovinati contiene un valore mostra il messaggio "Hai perso, hai indovinato solo un numero:" e il valore contenuiteo in numeri indovinati
+//      ALTRIMENTI mostrare il messaggio "Hai perso! Hai indovinato" lunghezza dell'array numeri indovinati "numeri! I numeri indovinati sono: "
+//          PER ogni numero di numeri indovinati stamapre a schermo il numero 
+
+
